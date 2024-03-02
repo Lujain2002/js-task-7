e9ed5c319c68d63e84fff3cdadf2563157b8dfba
Lujain2002 <130461892+Lujain2002@users.noreply.github.com>
2024-03-02 19:32:43 +0200

    مسبحة الكتورنية

---
+++ main.js
@@ -0,0 +1,26 @@
+document.querySelector(".btn1").addEventListener("click",()=>{
+   let counter1= document.querySelector(".counter1").innerHTML;
+   value1=parseInt(counter1);
+   value1=value1+1;
+   document.querySelector(".counter1").innerHTML=value1;
+});
+document.querySelector(".btn2").addEventListener("click",()=>{
+    let counter2= document.querySelector(".counter2").innerHTML;
+    value2=parseInt(counter2);
+    value2=value2+1;
+    document.querySelector(".counter2").innerHTML=value2;
+ });
+ document.querySelector(".btn3").addEventListener("click",()=>{
+    let counter3= document.querySelector(".counter3").innerHTML;
+    value3=parseInt(counter3);
+    value3=value3+1;
+    document.querySelector(".counter3").innerHTML=value3;
+ });
+ document.querySelector(".btn4").addEventListener("click",()=>{
+    let counter4= document.querySelector(".counter4").innerHTML;
+    value4=parseInt(counter4);
+    value4=value4+1;
+    document.querySelector(".counter4").innerHTML=value4;
+ });
+
+
